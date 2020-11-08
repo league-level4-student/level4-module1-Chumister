1ee37bcc881428970a66f65b530c8a64c8314a44
Chumister <alanchu4432@yahoo.com>
2020-11-08 11:28:04 -0800

    dsfgs

--- src/_04_Intro_To_Enums/StatesOfMatter.java
+++ src/_04_Intro_To_Enums/StatesOfMatter.java
@@ -1,5 +1,20 @@
 package _04_Intro_To_Enums;
 
-public enum StatesOfMatter {
-	SOLID, LIQUID, GAS, PLASMA, BOSE_EINSTEIN_CONDENSATE
+import _04_Intro_To_Enums.IntroToEnums.DataTypes;
+
+public class StatesOfMatter {
+	enum DataTypes{
+		SOLID, LIQUID, GAS, PLASMA, BOSE_EINSTEIN_CONDENSATE
+	}
+	public static void main(String[] args) {
+		DataTypes data = DataTypes.BOSE_EINSTEIN_CONDENSATE;
+		if (data == DataTypes.BOSE_EINSTEIN_CONDENSATE) {
+			System.out.println("Einstein");
+		}
+		switch(data) {
+		case BOSE_EINSTEIN_CONDENSATE:{
+			
+		}
+		}
+	}
 }
--- src/_05_Enum_Stuff/_00_Horoscope.java
+++ src/_05_Enum_Stuff/_00_Horoscope.java
@@ -5,12 +5,24 @@ import javax.swing.JOptionPane;
 public class _00_Horoscope {
 	// 1. Create an enum in a separate file called Zodiac that contains a category for
 	//    all 12 zodiac signs.
-	
+		enum Horo{
+			Aries, Leo, Cancer, Pisces, Scorpio, Taurus, Sagittaurus, Gemini, Virgo, Libra, Capricorn, Aquarius
+		}
 	// 2. Write a method that takes in a Zodiac enum object and uses a JOPtionPane to display
 	//    a different horoscope based on the Zodiac's state.
-	
+		
 	// 3. Make a main method to test your method
 	public static void main(String[] args) {
+		int h = JOptionPane.showOptionDialog(null, "Pick a zodiac", "Zodiac signs", 0,JOptionPane.INFORMATION_MESSAGE, null, Horo.values(), null);
 		
+		Horo data = Horo.Aquarius;
+		if (data == Horo.Aquarius) {
+			
+		}
+		switch(data) {
+		case Aries:{
+			
+		}
+		}
 	}
 }
